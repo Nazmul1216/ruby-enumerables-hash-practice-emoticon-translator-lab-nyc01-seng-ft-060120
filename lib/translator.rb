31930967b011be7692bcbd04509968cd567a4f5b
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-22 03:33:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -38,7 +38,7 @@ def get_english_meaning(file_path, emoticon)
   result=load_library(file_path)
   result.each do|emotion, symbols|
     if symbols[:japanese]==emoticon
-      eng_symbol=emotion[:english]
+      eng_symbol=emotion
     end 
   end
   eng_symbol
